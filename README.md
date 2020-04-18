214c87c5808c66d622f64a30c38d7f6a9d74611d
aksuman055 <aksuman055@gmail.com>
2020-04-18 15:24:03 +0530

    Added executable Resume.jar

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 daa-aksuman055 created by GitHub Classroom
 
 # Resume Builder
-A JAVA application for building resume.
+A JAVA application for building resume.</br></br>
+## Instructions
+1. Download/Clone the Resume.jar file
+2. Open Resume.jar and fill all the details.
+3. Click on Generate PDF, your CV will be saved by name your_name-CV.pdf in the same directory.</br>
 
 ![Resume Builder](https://github.com/nitcse2018/daa-aksuman055/blob/master/AWT%20Assignment/Resume/sss.PNG)
 ![Sample Resume Generated from application](https://github.com/nitcse2018/daa-aksuman055/blob/master/AWT%20Assignment/Resume/Resume.png)
---
+++ src/Resume/PDFTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Resume;
+import com.itextpdf.text.Document;
+
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+
+public class PDFTest {
+    public void createPdf(String name) throws IOException {
+        try (PDDocument document = new PDDocument()) {
+            PDPage blankPage = new PDPage();
+            document.addPage(blankPage);
+            document.save(name);
+        }
+        Document doc = new Document();
+    }
+
+    public static void main(String args[]) throws IOException {
+
+    }
+
+}
